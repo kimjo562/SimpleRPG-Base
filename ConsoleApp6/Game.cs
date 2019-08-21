497fe5b7da932519f8087a2f076e525117b3f6a4
Joseph Kim <s198022@students.aie.edu>
2019-08-21 09:15:01 -0500

    Add project files.

---
+++ ConsoleApp6/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.WriteLine("Welcome!");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+
+        }
+    }
+}
