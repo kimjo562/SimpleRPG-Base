37cc471b8037519c8818972d9123fef102788bb8
Joseph Kim <s198022@students.aie.edu>
2019-08-21 14:51:05 -0500

    Fixed Working Combat with Functions and Bool

--- ConsoleApp6/Game.cs
+++ ConsoleApp6/Game.cs
@@ -6,83 +6,59 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    class Game                                                          // Out of the Scope
+    class Game                                                                                                    // Out of the Scope
     {
         string playerName = "";
-        int playerHealth = 100;
-        int monsterHealth = 20;
-        int playerDamage = 10;
+        int playerHealth = 250;
+        int playerDamage = 20;
 
-        public void Start()                                                                        // In the Scope
+        public void Start()                                                                                       // In the Scope
         {
-            Welcome();                                                                             // Declaring the Function into use.
+            Welcome();                                                                                            // Declaring the Function into use.
             int monstersRemaining = 5;
             bool alive = true;
 
             // Fight until you lose.
             while (alive && monstersRemaining > 0)
             {
-                Console.WriteLine("There are " + monstersRemaining + " monsters remaining.");
-
-                alive = Encounter(20, 15);
-
+                Console.WriteLine("There are " + monstersRemaining + " monsters remaining.\n");
+                alive = Encounter(20, 60);                                                                        // First Digit is Monster Damage, Second Digit is Monster Health
                 monstersRemaining--;
-
             }
 
-            Console.ReadKey();                                                                      // system("pause") + cin
+            Console.ReadKey();                                                                                    // system("pause") + cin
         }
 
-             void Welcome()                                                                         // <---- These are Functions
-             {
-                    // Player Section
-                    Console.WriteLine("What is your name? ");
-                    playerName = Console.ReadLine();                                                // Similar to cin
-                    Console.WriteLine("Welcome " + playerName + "!\n");
+        void Welcome()                                                                                            // <---- These are Functions
+        { 
+            // Player Section
+            Console.WriteLine("What is your name? ");
+            playerName = Console.ReadLine();                                                                      // Similar to cin
+            Console.WriteLine("Welcome " + playerName + "!\n");
 
-             }
+        }
 
-             bool Encounter(int monsterDamage, int monsterHealth)                                                      // <---- These are Functions
-             {
-                        // Monster Section
-                    Console.WriteLine("A Monster has appeared!");
-                    string action = "";
-                 while (monsterHealth > 0 || playerHealth > 0)
-                 { 
-                    Console.WriteLine("What will you do?");
-                    Console.WriteLine("Use numbers to declare action or words.");
-                    Console.WriteLine("1.) Fight      2.) Flee\n");
-                    action = Console.ReadLine();
-              
-                if (action == "1" || action == "Fight" || action == "fight")                     // || = or
+        bool Encounter(int monsterDamage, int monsterHealth)                                                      // <---- These are Functions     Self Note: Order of variables does affect code)
+        {
+            // Monster Section
+            Console.WriteLine("A Monster has appeared!");
+            string action = "";
+            bool survived = true;
+
+            while (monsterHealth > 0 && playerHealth > 0)
+            {
+                Console.WriteLine("What will you do?");
+                Console.WriteLine("Use numbers to declare action or words.");
+                Console.WriteLine("1.) Fight      2.) Flee\n");
+                action = Console.ReadLine();
+
+                if (action == "1" || action == "Fight" || action == "fight")
                 {
-                   
-                        // Monster Attack
-                        Console.WriteLine("The Monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
-                        playerHealth -= monsterDamage;
-                        Console.WriteLine(playerName + " has " + playerHealth + " health remaining.");
-                        if (playerHealth <= 0)
-                        {
-                            Console.WriteLine(playerName + " was defeated.");
-                            return false;
-
-                        }
-
-                        // Player Attack
-                        Console.WriteLine("The monster takes " + playerDamage + " damage.");
-                        monsterHealth -= playerDamage;
-                        Console.WriteLine("The monster has " + monsterHealth + " health remaining.\n");
-                        if (monsterHealth < 0)
-                        {
-                            //Monster Defeat
-                            Console.WriteLine("The monster was defeated.");
-                            Console.WriteLine("Another monster has appeared!\n");
-
-
-
-
-                        }
-                    
+                    survived = Fight(ref monsterHealth, ref monsterDamage);
+                    if (!survived)
+                    {
+                        return false;
+                    }
                 }
 
                 else if (action == "2" || action == "Flee" || action == "flee")
@@ -92,12 +68,53 @@ namespace ConsoleApp6
                     return true;
                 }
 
+            }
+            return survived;
+        }
+        bool Fight(ref int monsterHealth, ref int monsterDamage)
+        {                                                                                                                                  // Beginning of bool Bracket
+            // Monster Attack
+            Console.WriteLine("The Monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
+            playerHealth -= monsterDamage;
+            Console.WriteLine(playerName + " has " + playerHealth + " health remaining.");
+            if (playerHealth <= 0)
+            {
+                Console.WriteLine(playerName + " was defeated.");
+                return false;
+
+            }
+
+            // Player Attack
+            Console.WriteLine("You attack!");
+            Console.WriteLine("The monster takes " + playerDamage + " damage.");
+            monsterHealth -= playerDamage;
+            Console.WriteLine("The monster has " + monsterHealth + " health remaining.\n");
+            if (monsterHealth <= 0)
+            {
+                //Monster Defeat
+                Console.WriteLine("The monster was defeated.");
+                //   Console.WriteLine("Another monster has appeared!\n");
+                return true;              
+            }
+            return true;
+        }                                                                                                                                     // End of the bool Bracket
+
+        bool Flee()
+        {
 
 
-                 }
-                return true;
-             }
-             
+
+            return true;
+        }
+
+        bool Heal()
+        {
+
+
+
+
+            return true;
+        }
 
     }
 }
