c7a54b3f0c430ebaab9d19673b7ca71145e4e371
Joseph Kim <s198022@students.aie.edu>
2019-08-21 10:13:36 -0500

    Turned Enounter into a Function

--- ConsoleApp6/Game.cs
+++ ConsoleApp6/Game.cs
@@ -6,54 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    class Game
+    class Game                                                          // Out of the Scope
     {
-        public void Start()
+        string playerName = "";
+        int playerHealth = 100;
+
+        public void Start()                                             // In the Scope
         {
             // Player Section
-            string playerName = "";
-            int playerHealth = 100;
-
             Console.WriteLine("What is your name? ");
             playerName = Console.ReadLine();                            // Similar to cin
             Console.WriteLine("Welcome " + playerName + "!\n");
+            Encounter();
 
-
-            // Monster Section
-            int monsterHealth = 100;
-            int monsterDamage = 10;
-            Console.WriteLine("A Monster has appeared!");
-
-            string action = "";
-            Console.WriteLine("What will you do?");
-            Console.WriteLine("Use numbers to declare action or words.");
-            Console.WriteLine("1.) Fight      2.) Flee\n");
-            action = Console.ReadLine();
-
-            if(action == "1" || action == "Fight" || action == "fight")                     // || = or
-            {
-                // Monster Attack
-                Console.WriteLine("The Monster attacks! " + playerName + " takes "  + monsterDamage + " damage!");
-                playerHealth -= monsterDamage;
-                Console.WriteLine("" + playerName + " has " + playerHealth + " remaining.");
-
-
-                // Player Attack
-                Console.WriteLine("You attack the monster.");
-
-
-            }
-            else if(action == "2" || action == "Flee" || action == "flee")
-            {
-                // Escape!
-                Console.WriteLine("You sucessfully escaped.");
-                
-
-
-
-
-
-            }
 
 
 
@@ -61,5 +26,49 @@ namespace ConsoleApp6
 
             Console.ReadKey();
         }
+
+             void Welcome()                                                                         // <---- These are Functions
+             {   
+
+
+             }
+
+             void Encounter()                                                                       // <---- These are Functions
+             {
+                        // Monster Section
+                    int monsterHealth = 100;
+                    int monsterDamage = 10;
+                    Console.WriteLine("A Monster has appeared!");
+
+                    string action = "";
+                    Console.WriteLine("What will you do?");
+                    Console.WriteLine("Use numbers to declare action or words.");
+                    Console.WriteLine("1.) Fight      2.) Flee\n");
+                    action = Console.ReadLine();
+
+                   if (action == "1" || action == "Fight" || action == "fight")                     // || = or
+                   {
+                        // Monster Attack
+                        Console.WriteLine("The Monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
+                        playerHealth -= monsterDamage;
+                        Console.WriteLine("" + playerName + " has " + playerHealth + " remaining.");
+
+
+                        // Player Attack
+                        Console.WriteLine("You attack the monster.");
+
+
+                   }
+
+                   else if (action == "2" || action == "Flee" || action == "flee")
+                   {
+                      // Escape!
+                        Console.WriteLine("You sucessfully escaped.");
+
+                   }
+
+             }
+
+
     }
 }
