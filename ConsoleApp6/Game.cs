18f73ec093d5464bff9b9376b7bd5c7c4988346f
Joseph Kim <s198022@students.aie.edu>
2019-08-21 09:50:09 -0500

    Added Fight and Action System.

--- ConsoleApp6/Game.cs
+++ ConsoleApp6/Game.cs
@@ -10,11 +10,56 @@ namespace ConsoleApp6
     {
         public void Start()
         {
-            Console.WriteLine("Welcome!");
-            Console.ReadLine();
+            // Player Section
+            string playerName = "";
+            int playerHealth = 100;
+
+            Console.WriteLine("What is your name? ");
+            playerName = Console.ReadLine();                            // Similar to cin
+            Console.WriteLine("Welcome " + playerName + "!\n");
+
+
+            // Monster Section
+            int monsterHealth = 100;
+            int monsterDamage = 10;
+            Console.WriteLine("A Monster has appeared!");
+
+            string action = "";
+            Console.WriteLine("What will you do?");
+            Console.WriteLine("Use numbers to declare action or words.");
+            Console.WriteLine("1.) Fight      2.) Flee\n");
+            action = Console.ReadLine();
+
+            if(action == "1" || action == "Fight" || action == "fight")                     // || = or
+            {
+                // Monster Attack
+                Console.WriteLine("The Monster attacks! " + playerName + " takes "  + monsterDamage + " damage!");
+                playerHealth -= monsterDamage;
+                Console.WriteLine("" + playerName + " has " + playerHealth + " remaining.");
+
+
+                // Player Attack
+                Console.WriteLine("You attack the monster.");
+
+
+            }
+            else if(action == "2" || action == "Flee" || action == "flee")
+            {
+                // Escape!
+                Console.WriteLine("You sucessfully escaped.");
+                
 
 
 
+
+
+            }
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
